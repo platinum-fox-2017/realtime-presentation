5a23d77cabb8e66633eebfa43ab8d463a272d5bf
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-21 19:58:56 +0700

    Change host when access online

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ var server = require('http').createServer(app);
 const io = require('socket.io')(server)
 
 io.on('connection', function(client){
+  console.log('new connection')
   client.on('move-slide',function(data) {
+    console.log('move-slide')
     io.emit('move-slide',data)
   })
 });
