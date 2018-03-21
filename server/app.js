0b1c4e2a50a51371541d8aa10dab0c513000fa89
herby herado <herby.herado@gmail.com>
2018-03-21 15:52:08 +0700

    add working directory

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const PORT = process.env.PORT || 3000
+
+io.on('connection', (socket) => {
+//   console.log(socket)
+  socket.emit('welcome')
+  socket.on('patty', (value) => {
+    console.log('masuk kesini')
+    // console.log()
+    socket.emit('patty', value)
+    // console.log(value)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log('server is running on port ' + PORT)
+})
